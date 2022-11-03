fabe0a1518ef5556b7faa81e540a2a8e9669c901
Miguel Shinyenyi <miguel.amwayi.shinyenyi@gmail.com>
2022-11-03 15:06:36 +0300

    edit functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 import addNewTask from '../modules/addTask.js';
 import deleteTask from '../modules/deleteTask.js';
+import editTask from '../modules/editTask.js';
 
 const taskInput = document.querySelector('.input-task');
 
@@ -20,7 +21,7 @@ const createTasksList = (tasks) => {
         <li class="task-item">
           <div class="task-item-div">
             <input class="task-checkbox" type="checkbox" name="" id="" ${checked}>
-            <p class="task-name">${task.description}</p>
+            <input id="edit-input" class="task-name" type="text" name="${task.index}" value="${task.description}">
           </div>
           <i id="${task.index}" class="fa fa-trash task-menu" aria-hidden="true"></i>
         </li>
@@ -43,6 +44,14 @@ tasksListSection.addEventListener('click', (event) => {
   if (event.target.id * 1 === parseInt(event.target.id, 10)) {
     tasks = Array.from(JSON.parse(localStorage.getItem('tasks') || '[]'));
     deleteTask(event, tasks);
+    createTasksList(tasks);
+  }
+});
+
+tasksListSection.addEventListener('keydown', (event) => {
+  if (event.target.id === 'edit-input' && event.target.value && event.code === 'Enter') {
+    tasks = Array.from(JSON.parse(localStorage.getItem('tasks') || '[]'));
+    editTask(event, tasks);
     createTasksList(tasks);
   }
 });
